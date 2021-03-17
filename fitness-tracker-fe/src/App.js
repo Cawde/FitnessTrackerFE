43760a18f9cb174f458411a5a0325f918e29ce80
Aaron Pollack <apollack0722@gmail.com>
2021-03-16 20:32:51 -0500

    navLink component stage 1

--- fitness-tracker-fe/src/App.js
+++ fitness-tracker-fe/src/App.js
@@ -1,4 +1,4 @@
-
+import 'NavLink' 
 import './App.css';
 
 function App() {
---
+++ fitness-tracker-fe/src/Components/NavLink.js
@@ -0,0 +1,15 @@
+
+
+const NavLink = () => {
+    return (
+        <div className="nav_buttons">
+            <ul>
+                <li>Home</li>
+                <li>Activities</li>
+                <li>Routines</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavLink;
