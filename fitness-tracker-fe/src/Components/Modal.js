fb3ff926a0caf6ce783b7ace9622c89b2b235f13
Aaron Pollack <apollack0722@gmail.com>
2021-03-20 02:02:56 -0500

    submit button added to modal

--- fitness-tracker-fe/src/Components/Modal.js
+++ fitness-tracker-fe/src/Components/Modal.js
@@ -1,34 +1,74 @@
 import { useState } from 'react'
-import {  Dialog, DialogActions, DialogContent } from '@material-ui/core';
+import {  Dialog, DialogActions, DialogContent, TextField } from '@material-ui/core';
 
 const Modal = (modalTitle) => {
-  const [ modalDisplay, setModalDisplay ] = useState(false); 
-  return (
-    <>
-      <div 
-        className="actionButton" 
-        onClick={()=>setModalDisplay(true)}
-      >Open Modal
-      </div>
-      {modalDisplay ? 
-      <Dialog
-        open={modalDisplay}
-        className='actionModal'
-        onClose={() => setModalDisplay(false)}
-      >
-        <DialogContent
-          >test this thing
-        </DialogContent>
-        <DialogActions>
-          <div 
+    const [ modalDisplay, setModalDisplay ] = useState(false); 
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [count, setCount] = useState('');
+    return (
+        <>
+        <div 
             className="actionButton" 
-            onClick={()=>{setModalDisplay(false)}}
-          >Close Modal
-          </div>
-        </DialogActions>
-       
-      </Dialog> : null} 
-    </>)
+            onClick={()=>setModalDisplay(true)}
+        >Open Modal
+        </div>
+        {modalDisplay ? 
+        <Dialog
+            open={modalDisplay}
+            className='actionModal'
+            onClose={() => setModalDisplay(false)}
+        >
+            <DialogContent
+                classname="modalContent"
+            >
+                    <TextField
+                        autoFocus
+                        id="Title"
+                        label="Title"
+                        type="text"
+                        fullWidth
+                        value={title}
+                        onChange={(event) => setTitle(event.target.value)}
+                        
+                    />
+                    <TextField
+                        autoFocus
+                        id="Description"
+                        label="Description"
+                        type="text"
+                        fullWidth
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                        
+                    />
+                    <TextField
+                        autoFocus
+                        id="Count"
+                        label="Count"
+                        type="text"
+                        fullWidth
+                        value={count}
+                        onChange={(event) => setCount(event.target.value)}  
+                    />
+                    <button 
+                        className="actionButton" 
+                        type="submit" 
+                        value="Submit"
+                    >Submit
+                    </button>
+
+            </DialogContent>
+            <DialogActions>
+            <div 
+                className="actionButton" 
+                onClick={()=>{setModalDisplay(false)}}
+            >Close Modal
+            </div>
+            </DialogActions>
+        
+        </Dialog> : null} 
+        </>)
 }
 export default Modal;
 
